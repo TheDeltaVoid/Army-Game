8aca512f642da1ee028e2999f2ad763b622425d8
LuluEmc2 <lucien.vite@proton.me>
2025-04-08 21:45:38 +0200

    Changed font name in constants and menu.

--- modules/constants.py
+++ modules/constants.py
@@ -11,6 +11,7 @@ TITLE = "Army Game"
 BG_COLOR = pyray.Color(12, 23, 45, 255)
 
 TITLE_COLOR = pyray.Color(246, 254, 251, 255)
-TITLE_FONT = "resources/fonts/Format_1452/fonts/format_1452_web/format_1452-webfont.ttf"
 TITLE_FONT_SIZE = 70
 TITLE_SPACING = 0
+
+FONT_FORMAT_1452 = "resources/fonts/Format_1452/fonts/format_1452_web/format_1452-webfont.ttf"
--- modules/scenes/menus.py
+++ modules/scenes/menus.py
@@ -11,8 +11,9 @@ class MainMenu:
         pass
 
     def render(self):
-        title_font = pyray.load_font_ex(TITLE_FONT, TITLE_FONT_SIZE, None, 0)
-        text_size = pyray.measure_text_ex(title_font, get_text("TITLE"), TITLE_FONT_SIZE, TITLE_SPACING)
+        font_format_1452 = pyray.load_font_ex(FONT_FORMAT_1452, TITLE_FONT_SIZE, None, 0)
+        
+        text_size = pyray.measure_text_ex(font_format_1452, get_text("TITLE"), TITLE_FONT_SIZE, TITLE_SPACING)
         print(text_size.x)
         text_pos = pyray.Vector2((pyray.get_render_width() // 2) - (text_size.x // 2), (text_size.y // 2))
-        pyray.draw_text_ex(title_font, get_text("TITLE"), text_pos, TITLE_FONT_SIZE, TITLE_SPACING, TITLE_COLOR)
+        pyray.draw_text_ex(font_format_1452, get_text("TITLE"), text_pos, TITLE_FONT_SIZE, TITLE_SPACING, TITLE_COLOR)
