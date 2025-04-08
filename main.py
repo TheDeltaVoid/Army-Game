e4f59d640f153c9344573be8245f6d3c07a7d1ec
TheDeltaVoid <simonkrey2009@icloud.com>
2025-04-08 18:01:13 +0200

    used scene_manager and delta_time

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from pyray import *
+
 from modules.constants import *
+from modules.scene_manager import SceneManager
 
 def main():
     init_window(WIDTH, HEIGHT, TITLE)
 
+    scene_manager = SceneManager()
+
+    delta_time = 0
+
     while not window_should_close():
+        delta_time = get_frame_time()
+
+        scene_manager.update(delta_time)
+
         begin_drawing()
         clear_background(BLACK)
+
+        scene_manager.render()
 
         end_drawing()
 
---
+++ modules/scene_manager.py
@@ -0,0 +1,11 @@
+from pyray import *
+
+class SceneManager:
+    def __init__(self):
+        self.current = "main_menu"
+
+    def update(delta_time):
+        pass
+
+    def render():
+        pass
