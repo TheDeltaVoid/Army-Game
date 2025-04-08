b147716a53f6deb8b54f22c7cc22eadc42a481d6
LuluEmc2 <lucien.vite@proton.me>
2025-04-08 21:56:46 +0200

    Changed menu colors.

--- modules/constants/menu.py
+++ modules/constants/menu.py
@@ -1,7 +1,7 @@
 import pyray
 
-BG_COLOR = pyray.Color(12, 23, 45, 255)
+BG_COLOR = pyray.Color(30, 27, 26, 255)
 
-TITLE_COLOR = pyray.Color(246, 254, 251, 255)
+TITLE_COLOR = pyray.Color(195, 198, 188, 255)
 TITLE_FONT_SIZE = 70
 TITLE_SPACING = 0
