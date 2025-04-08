183d4ccfbdba3036c99163de2425012381972b06
TheDeltaVoid <simonkrey2009@icloud.com>
2025-04-08 18:21:18 +0200

    addet menus

--- modules/scene_manager.py
+++ modules/scene_manager.py
@@ -1,11 +1,20 @@
 from pyray import *
 
+from modules.menu.main import MainMenu
+
 class SceneManager:
     def __init__(self):
-        self.current = "main_menu"
+        self.current = "menu"
+        self.current_menu = "main"
+        
+        self.menus = {
+            "main" : MainMenu()
+        }
 
     def update(self, delta_time):
-        pass
+        if self.current == "menu":
+            self.menus[self.current_menu].update(delta_time)
 
     def render(self):
-        pass
+        if self.current == "menu":
+            self.menus[self.current_menu].render()
