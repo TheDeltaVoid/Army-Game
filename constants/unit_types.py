488d13a9f9a94a7cc25b26fe7762e77abdccfaf0
LuluEmc2 <lucien.vite@proton.me>
2025-04-10 19:43:38 +0200

    Added new unit types.

--- constants/unit_types.py
+++ constants/unit_types.py
@@ -7,4 +7,44 @@ INFANTRY = unit.UnitType(150,
                          70,
                          "resources/units/infantry.jpg")
 
-ARMY_COMPOSITION = [INFANTRY, INFANTRY, INFANTRY]
+AMPHIBIAN_INFANTRY = unit.UnitType(50,
+                         50,
+                         20,
+                         0,
+                         60,
+                         "resources/units/amphibian_infantry.jpg")
+
+MOUNTAIN_INFANTRY = unit.UnitType(80,
+                         50,
+                         30,
+                         60,
+                         100,
+                         "resources/units/mountain_infantry.jpg")
+
+NAVAL_UNIT = unit.UnitType(60,
+                         40,
+                         40,
+                         0,
+                         30,
+                         "resources/units/naval_unit.jpg")
+
+RECONNAISSANCE = unit.UnitType(90,
+                         100,
+                         10,
+                         30,
+                         80,
+                         "resources/units/reconnaissance.jpg")
+
+ARMY_COMPOSITION = [INFANTRY,
+                    INFANTRY,
+                    INFANTRY,
+                    INFANTRY,
+                    AMPHIBIAN_INFANTRY,
+                    AMPHIBIAN_INFANTRY,
+                    MOUNTAIN_INFANTRY,
+                    MOUNTAIN_INFANTRY,
+                    NAVAL_UNIT,
+                    NAVAL_UNIT,
+                    RECONNAISSANCE,
+                    RECONNAISSANCE
+                    ]
